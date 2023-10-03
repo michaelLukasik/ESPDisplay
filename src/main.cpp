d842519eb91795327617485186a4b16df0744117
michaelLukasik <mlukasik17@gmail.com>
2023-10-02 19:37:27 -0500

    Start accelerometer project

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "BluetoothSerial.h"
 #include "ELMduino.h" // Reminder: debugMode is OFF!
 #include "bunArraysColor.h"
-#include <Arduino_GFX_Library.h>
+#include <Arduino_GFX_Library.h> // For the love of god cull this library
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
 
 BluetoothSerial SerialBT;
@@ -15,13 +17,16 @@ BluetoothSerial SerialBT;
 #define TFT_CS     15
 #define TFT_DC     4
 #define TFT_RESET  2
-
+//#define I2C_SCL 22  Commented just because these happen to be defaults anyways
+//#define I2c_SDA 21
+const int ADXL345 = 0x53;
 
 /// Display Settings 
 // Display = ILI9341  ---> https://github.com/adafruit/Adafruit_ILI9341/blob/master/Adafruit_ILI9341.h
 // Dimensions = 240px L  x 320px H
 Arduino_ESP32SPI bus = Arduino_ESP32SPI(TFT_DC, TFT_CS, TFT_SCK, TFT_MOSI, TFT_MISO);
 Arduino_ILI9341 display = Arduino_ILI9341(&bus, TFT_RESET);
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
 ELM327 myELM327;
 
@@ -35,6 +40,7 @@ uint16_t* buns[] = {
 TaskHandle_t cycleScreen; 
 uint8_t address[6] = { 0x66, 0x1E, 0x32, 0xF8, 0xC3, 0xA1 };
 
+float X_out, Y_out, Z_out;  // Outputs from ADXL345 
 
 uint32_t rpm  = 0;
 float oilTemp = 0;
@@ -52,9 +58,39 @@ volatile float cachedEFR = -1;
 float tempEFR = 0;
 
 
+float accelXOffset = 0.35;
+float accelYOffset = 0.35;
+float AccelMaxX = 0;
+float AccelMaxY = 0;
+float AccelMaxZ = 0;
+
 //task1: Cycle through Screen Updates
 void cycleScreenCode ( void *pvParameters ){
 for(;;){
+
+
+  /* Get a new sensor event */ 
+  sensors_event_t accelEvent;  
+  accel.getEvent(&accelEvent);
+    
+  if (accelEvent.acceleration.x > AccelMaxX) AccelMaxX = accelEvent.acceleration.x; 
+  if (accelEvent.acceleration.y > AccelMaxY) AccelMaxY = accelEvent.acceleration.y;
+  if (accelEvent.acceleration.z > AccelMaxZ) AccelMaxZ = accelEvent.acceleration.z;
+  
+  Serial.print("Accel: "); 
+  Serial.print((accelEvent.acceleration.x - accelXOffset) / 9.81); 
+  Serial.print("  ");
+  Serial.print((accelEvent.acceleration.y - accelYOffset) / 9.81); 
+  Serial.print("  "); 
+  Serial.print(accelEvent.acceleration.z / 9.81); 
+  Serial.println();
+  Serial.print("Accel Maximums: "); 
+  Serial.print(AccelMaxX); 
+  Serial.print("  ");
+  Serial.print(AccelMaxY); 
+  Serial.print("  "); 
+  Serial.print(AccelMaxZ); 
+  Serial.println();
 
   if (bunFrame > 9) {bunFrame = 0;}
   // Bunny Display
@@ -105,14 +141,6 @@ for(;;){
 void setup()
 {
   DEBUG_PORT.begin(115200);
-  xTaskCreatePinnedToCore(
-      cycleScreenCode, /* Function to implement the task */
-      "cycleScreen", /* Name of the task */
-      10000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      0,  /* Priority of the task */
-      &cycleScreen,  /* Task handle. */
-      0); /* Core where the task should run */ 
 
   /// Set up Display ////
   display.begin();
@@ -142,6 +170,23 @@ void setup()
   display.setTextSize(1);
   display.print(F("l/h"));  
 
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+  accel.setRange( ADXL345_RANGE_4_G);
+
+  xTaskCreatePinnedToCore(
+    cycleScreenCode, /* Function to implement the task */
+    "cycleScreen", /* Name of the task */
+    10000,  /* Stack size in words */
+    NULL,  /* Task input parameter */
+    0,  /* Priority of the task */
+    &cycleScreen,  /* Task handle. */
+    0); /* Core where the task should run */ 
 
   ELM_PORT.begin("ml.ESP32",true);
   
