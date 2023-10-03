2a8f37ca7ecb77b801a4de026034e68bb0a8fa46
michaelLukasik <mlukasik17@gmail.com>
2023-10-03 16:23:24 -0500

    Changed to eSPI, bugs in older lib with FillScreen

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "BluetoothSerial.h"
 #include "ELMduino.h" // Reminder: debugMode is OFF!
-#include "bunArraysColor.h"
-#include <Arduino_GFX_Library.h>
+#include "bunArraysX.h"
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+TFT_eSPI display = TFT_eSPI();
 
 
 BluetoothSerial SerialBT;
@@ -9,32 +12,31 @@ BluetoothSerial SerialBT;
 #define DEBUG_PORT Serial
 
 /// Make sure these connections are correct ///
-#define TFT_SCK    18
-#define TFT_MOSI   23
-#define TFT_MISO   29
-#define TFT_CS     15
-#define TFT_DC     4
-#define TFT_RESET  2
+//#define TFT_SCK    18
+//#define TFT_MOSI   23
+//#define TFT_MISO   19
+//#define TFT_CS     15
+//#define TFT_DC     4
+//#define TFT_RESET  2
 
 
 /// Display Settings 
 // Display = ILI9341  ---> https://github.com/adafruit/Adafruit_ILI9341/blob/master/Adafruit_ILI9341.h
 // Dimensions = 240px L  x 320px H
-Arduino_ESP32SPI bus = Arduino_ESP32SPI(TFT_DC, TFT_CS, TFT_SCK, TFT_MOSI, TFT_MISO);
-Arduino_ILI9341 display = Arduino_ILI9341(&bus, TFT_RESET);
+//Arduino_ESP32SPI bus = Arduino_ESP32SPI(TFT_DC, TFT_CS, TFT_SCK, TFT_MOSI, TFT_MISO);
+//Arduino_ILI9341 display = Arduino_ILI9341(&bus, TFT_RESET);
 
 ELM327 myELM327;
 
 // Counter for bunny images :0) //
 int bunFrame = 0;
 
-uint16_t* buns[] = {
+const unsigned char* buns[] = {
     bun1, bun2, bun3, bun4, bun5, bun6, bun7, bun8, bun9, bun10, 
 };
 
 TaskHandle_t cycleScreen; 
 uint8_t address[6] = { 0x66, 0x1E, 0x32, 0xF8, 0xC3, 0xA1 };
-
 
 uint32_t rpm  = 0;
 float oilTemp = 0;
@@ -51,137 +53,133 @@ volatile float EFR = 0; // Engine Fuel Rate
 volatile float cachedEFR = -1;
 float tempEFR = 0;
 
+int frameInterval = 50;
+volatile unsigned long time_now = millis();
+int BTLock = 0;
+
 
 //task1: Cycle through Screen Updates
 void cycleScreenCode ( void *pvParameters ){
 for(;;){
-
   if (bunFrame > 9) {bunFrame = 0;}
   // Bunny Display
-  display.draw16bitBeRGBBitmap(0, 273, buns[bunFrame], 64, 47);
-  if (MPH <= 25) { delay(250);}
-  else if (MPH > 25 and MPH <= 40) { delay(150);}
-  else if (MPH > 40 and MPH <= 65) { delay(50);}
-  else if (MPH > 65) { delay(10);}
-  else { delay(10); }
-  bunFrame +=1;
-  
+  display.drawXBitmap(0, 273, buns[bunFrame], 64, 47, TFT_WHITE);
+  if (MPH <= 25) { frameInterval = 250;}
+  else if (MPH > 25 and MPH <= 40) { frameInterval = 150;}
+  else if (MPH > 40 and MPH <= 65) { frameInterval = 50;}
+  else if (MPH > 65) { frameInterval = 20;}
+  if(millis() - time_now > frameInterval){
+    display.drawXBitmap(0, 273, buns[bunFrame], 64, 47, TFT_BLACK); // Draw over old bitmap with background color to "erase" and prime for next image
+    bunFrame +=1;
+    time_now = millis();
+  }
+
   // Update Mph next to the bun
   if(cachedMPH != MPH){
-    display.fillRect(70, 273 , 170 , 30, BLACK);
+    display.fillRect(70, 273 , 170 , 30, TFT_BLACK);
     display.setCursor(75,282);
     display.setTextSize(3);
-    display.setTextColor(GREEN);
+    display.setTextColor(TFT_GREEN);
     display.print(F("MPH: "));
     display.print(int(MPH));
     cachedMPH = MPH;
-  }
+    }
   // Update pedal position value, bar (X=5,Y=50, H=200, W=30) fills from bottom with Green
   if(cachedRelativeThrottle != relativeThrottle){
-    display.fillRect(5, 50 , 30 , 2*(100 - relativeThrottle), BLACK);
-    display.fillRect(5, 50 + 2*(100 - relativeThrottle), 30 , (200*relativeThrottle)/100 , GREEN); // Draw Rectangle as a percentage of how pressed the pedal is
-    display.drawRect(5, 50 , 30 , 200, RED);
+    display.fillRect(5, 50 , 30 , 2*(100 - relativeThrottle), TFT_BLACK);
+    display.fillRect(5, 50 + 2*(100 - relativeThrottle), 30 , (200*relativeThrottle)/100 , TFT_GREEN); // Draw Rectangle as a percentage of how pressed the pedal is
+    display.drawRect(5, 50 , 30 , 200, TFT_RED);
     display.setCursor(15,140);
     display.setTextSize(1);
-    display.setTextColor(PINK);
+    display.setTextColor(TFT_PINK);
     display.print(int(relativeThrottle));
     cachedRelativeThrottle = relativeThrottle;
     }
   // Update Fuel Rate, this needs some testing
   if(cachedEFR != EFR){
-  display.fillRect(40, 233 , 200 , 30, BLACK);
-  display.setCursor(45,242);
-  display.setTextSize(3);
-  display.setTextColor(LIGHTGREY);
-  display.print(F("EFR: "));
-  display.print(EFR,1);
-  display.setTextSize(1);
-  display.print(F("l/h"));  
-  cachedEFR = EFR;
+    display.fillRect(40, 233 , 200 , 30, TFT_BLACK);
+    display.setCursor(45,242);
+    display.setTextSize(3);
+    display.setTextColor(TFT_LIGHTGREY);
+    display.print(F("EFR: "));
+    display.print(EFR,1);
+    display.setTextSize(1);
+    display.print(F("l/h"));  
+    cachedEFR = EFR;
     }
   } // End of core 0 loop code
 }
 
-void setup()
-{
+void setup(){
   DEBUG_PORT.begin(115200);
-  xTaskCreatePinnedToCore(
-      cycleScreenCode, /* Function to implement the task */
-      "cycleScreen", /* Name of the task */
-      10000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      0,  /* Priority of the task */
-      &cycleScreen,  /* Task handle. */
-      0); /* Core where the task should run */ 
-
+  
   /// Set up Display ////
   display.begin();
-  display.fillScreen(BLACK);
+  display.fillScreen(TFT_BLACK);
   display.setCursor(20, 20);
   display.setTextSize(2);
-  display.setTextColor(WHITE);
+  display.setTextColor(TFT_WHITE);
   display.print(F("Connecting!!!"));
 
   // Set up pedal bar
   display.setTextSize(1);
-  display.setTextColor(PINK);
+  display.setTextColor(TFT_PINK);
   display.setCursor(15,135);
   display.print(int(relativeThrottle));
-  display.drawRect(5, 50 , 30 , 200, RED);
+  display.drawRect(5, 50 , 30 , 200, TFT_RED);
   display.setCursor(5,255);
   display.print(F("Thr.")); 
 
   //Set up EFR area 
   //display.drawRect(40, 233 , 200 , 32, RED);
-  display.fillRect(40, 233 , 200 , 30, BLACK);
+  display.fillRect(40, 233 , 200 , 30, TFT_BLACK);
   display.setCursor(45,242);
   display.setTextSize(3);
-  display.setTextColor(LIGHTGREY);
+  display.setTextColor(TFT_LIGHTGREY);
   display.print(F("EFR: "));
   display.print(EFR,1);
   display.setTextSize(1);
-  display.print(F("l/h"));  
-
+  display.print(F("l/h"));   
 
   ELM_PORT.begin("ml.ESP32",true);
   
   if (!ELM_PORT.connect(address)){
     DEBUG_PORT.println(F("Couldn't connect to OBD scanner - Phase 1"));
-    display.fillRect(0, 0 , 240 , 40, BLACK);
     display.setCursor(20, 40);
     display.setTextSize(2);
-    display.setTextColor(WHITE);
+    display.setTextColor(TFT_WHITE);
     display.print(F("Not connected (Code 1)"));
     display.setCursor(20, 80);
     display.setTextSize(2);
-    display.setTextColor(WHITE);
+    display.setTextColor(TFT_WHITE);
     display.print(F("Reset the module dumbass"));
     display.setCursor(70,282);
     display.setTextSize(3);
-    display.setTextColor(RED);
+    display.setTextColor(TFT_RED);
     display.print(F("MPH: N/A "));
-    while(1);
+    BTLock = 1;
   }
 
-  if (!myELM327.begin(ELM_PORT, 0, 1000)){
-    Serial.println(F("Couldn't connect to OBD scanner - Phase 2"));
-    display.setCursor(20, 40);
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.print(F("Not connected (Code 2)"));
-    display.setCursor(20, 80);
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    display.print(F("Reset the module dumbass"));
-    while (1);
+  if(BTLock == 0){
+    display.fillScreen(TFT_BLACK);
+    display.fillRect(0, 0 , 240 , 40, TFT_BLACK);
+    display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.setTextColor(TFT_NAVY);
+    display.print(F("BT Connected!"));
   }
-  display.fillRect(0, 0 , 240 , 40, BLACK);
-  display.setCursor(0, 0);
-  display.setTextSize(1);
-  display.setTextColor(NAVY);
-  display.print(F("BT Connected!"));
+
+  xTaskCreatePinnedToCore(
+    cycleScreenCode, /* Function to implement the task */
+    "cycleScreen", /* Name of the task */
+    10000,  /* Stack size in words */
+    NULL,  /* Task input parameter */
+    0,  /* Priority of the task */
+    &cycleScreen,  /* Task handle. */
+    0); /* Core where the task should run */
+
+  while(BTLock == 1){}
 }
-
 
 void loop()
 {
